1f01bb46dcd5c7797abd898de1345d4d6c3ba6c6
admln52 <ismrwang@gmail.com>
2016-12-18 14:28:25 +0800

    增加学生信息管理

--- js/additional-methods.js
+++ js/additional-methods.js
@@ -1,3 +1,6 @@
 jQuery.validator.addMethod("isUserName", function (value, element) {
     return this.optional(element) || /^[a-zA-z][a-zA-Z0-9_]{1,17}$/.test(value);
 });
+jQuery.validator.addMethod("notSpecialString", function (value, element) {
+    return this.optional(element) || /^([\u4e00-\u9fa5]+|[a-zA-Z0-9]+)$/.test(value);
+});
--- js/person-admin.js
+++ js/person-admin.js
@@ -16,3 +16,41 @@ $(".js-band").on("click", function (e) {
 $(".js-del").on("click", function (e) {
     $('#confirmDel').modal('show');
 })
+
+
+$("#list").on("click", function (e) {
+    if(e.target && e.target.nodeName.toLowerCase() == "button") {
+        e.target.parentNode.parentNode.remove();
+    }
+})
+$("#addNewItem").on("click", function () {
+    var innerHTML = '<tr>'
+        + '<td><input class="form-control input-sm" type="text" name="ids" placeholder="输入学号">'
+        + '</td>'
+        + '<td><input class="form-control input-sm" type="text" name="names" placeholder = "输入姓名"></td> '
+        + '<td>'
+        + '<button type="button" class="btn btn-danger btn-xs">删除</button>'
+        + '</td>'
+        + '</tr>';
+    $("#list").html($("#list").html() +innerHTML);
+})
+
+var validator = $("#addStudent").validate({
+    rules: {
+        ids: {
+            notSpecialString: true
+        },
+        names: {
+            notSpecialString: true
+        }
+    },
+    messages: {
+        ids: {
+            notSpecialString: "不能输入特殊字符"
+        },
+        names: {
+            notSpecialString: "不能输入特殊字符"
+        }
+    }
+
+});
